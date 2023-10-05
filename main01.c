2e0850a38ee62d6f0b6a690c39ac0009af87d5c5
subinkim32 <salgeum1222@gmail.com>
2023-10-05 16:17:32 +0900

    main01

--- main01.c
+++ main01.c
@@ -4,16 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int num;
+	char c;
+	int num = 0;
 	
-	printf("input a number : \n");
-	scanf("%d", &num);
+	printf("input a string : ");
 	
-	if ( num < 0 )
-		printf("%d", -num); 	
-	else
-		printf("%d", num);
-			
+	while ( ( c = getchar() ) != '\n' )
+	{
+		if ( c >= '0' && c <= '9' )
+			num++;	
+	}
+	
+	printf("Number of numbers in the input string is %d.\n", num);
 	
 	system("PAUSE");
 	return 0;
